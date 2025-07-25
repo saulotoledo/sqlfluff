f6d33419700529bcc0c0ddaebd2f85147195f20f
Saulo Soares de Toledo <saulotoledo@gmail.com>
2025-07-26 00:54:05 +0200

    Oracle: Add EXECUTE IMMEDIATE statement support

--- src/sqlfluff/dialects/dialect_oracle.py
+++ src/sqlfluff/dialects/dialect_oracle.py
@@ -87,6 +87,7 @@ oracle_dialect.sets("reserved_keywords").update(
         "ELSE",
         "EXCLUSIVE",
         "EXISTS",
+        "EXECUTE",
         "FILE",
         "FLOAT",
         "FOR",
@@ -1072,6 +1073,7 @@ class StatementSegment(ansi.StatementSegment):
             Ref("AssignmentStatementSegment"),
             Ref("RecordTypeDefinitionSegment"),
             Ref("DeclareCursorVariableSegment"),
+            Ref("ExecuteImmediateSegment"),
             Ref("FunctionSegment"),
             Ref("IfExpressionStatement"),
             Ref("CaseExpressionSegment"),
@@ -1902,6 +1904,34 @@ class DeclareCursorVariableSegment(BaseSegment):
         ),
         Sequence("IS", Ref("SelectStatementSegment"), optional=True),
         Ref("DelimiterGrammar", optional=True),
+    )
+
+
+class ExecuteImmediateSegment(BaseSegment):
+    """An `EXECUTE IMMEDIATE` statement.
+
+    https://docs.oracle.com/en/database/oracle/oracle-database/23/lnpls/EXECUTE-IMMEDIATE-statement.html
+    """
+
+    type = "execute_immediate_statement"
+
+    match_grammar = Sequence(
+        "EXECUTE",
+        "IMMEDIATE",
+        Ref("ExpressionSegment"),
+        Sequence(
+            "INTO",
+            Delimited(
+                Ref("ColumnReferenceSegment"),
+                Ref("SingleIdentifierGrammar"),
+            ),
+            optional=True,
+        ),
+        Sequence(
+            "USING",
+            Delimited(Ref("ExpressionSegment")),
+            optional=True,
+        ),
     )
 
 
